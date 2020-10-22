a57b5ddac4f0e282ad645c0f11930aa387293378
Adam Coogan <dr.adam.coogan@gmail.com>
2020-10-22 13:51:32 +0200

    hopefully fix cwd bug

--- src/clipppy/yaml.py
+++ src/clipppy/yaml.py
@@ -295,7 +295,11 @@ class MyYAML(yaml.YAML):
         path = (not isinstance(path_or_stream, io.IOBase)) and Path(path_or_stream)
         stream = path and path.open() or path_or_stream
 
-        with cwd(os.path.split(stream.name)[0]):
+        wd = os.path.split(stream.name)[0]
+        if wd == "":
+            wd = "./"
+
+        with cwd(wd):
             return super().load(stream)
 
     def __init__(self, base_dir: os.PathLike = None):
