af59cf979076e80ef7f0b3ad16b7dba8f19561b8
Kosio Karchev <kosiokarchev@gmail.com>
2021-10-18 18:39:18 +0200

    block correlations in HPMVN

--- src/clipppy/guide/sampling_groups/hpmvn.py
+++ src/clipppy/guide/sampling_groups/hpmvn.py
@@ -4,6 +4,7 @@ from typing import Mapping
 
 import torch
 from more_itertools import all_equal
+from pyro.distributions.constraints import lower_cholesky
 from pyro.nn import PyroParam
 from torch import Size
 
@@ -11,7 +12,7 @@ from .pmvn import PartialMultivariateNormalSamplingGroup
 
 
 class HierarchicPartialMultivariateNormalSamplingGroup(PartialMultivariateNormalSamplingGroup):
-    def __init__(self, *args, hdims: Mapping[str, int], **kwargs):
+    def __init__(self, *args, hdims: Mapping[str, int], init_scale_const_corr=1., **kwargs):
         super().__init__(*args, **kwargs)
 
         hdims = {name: len(self.shapes[name]) - nevent for name, nevent in hdims.items()}
@@ -21,6 +22,7 @@ class HierarchicPartialMultivariateNormalSamplingGroup(PartialMultivariateNormal
         self.corr_batch = {name: self.shapes[name][:nevent] for name, nevent in hdims.items()}
         assert all_equal(self.corr_batch.values())
         self.corr_batch: Size = next(iter(self.corr_batch.values()))
+        assert len(self.corr_batch) > 0
 
         self.corr_sizes = {name: self.shapes[name][nevent:].numel() for name, nevent in hdims.items()}
         self.corr_size: int = sum(self.corr_sizes.values())
@@ -36,13 +38,33 @@ class HierarchicPartialMultivariateNormalSamplingGroup(PartialMultivariateNormal
             for name in self.corr_names
         ), -1)
 
+        self.init_scale_const_corr = init_scale_const_corr
+
+    @PyroParam(constraint=lower_cholesky, event_dim=2)
+    def const_corr(self):
+        # TODO: simplify const_corr_scale jacobian?
+        return self._scale_diagonal(
+            self.init_scale_const_corr, torch.cat(tuple(
+                torch.atleast_1d(tr.log_abs_det_jacobian(z, tr(z)).exp()).flatten()
+                for name in self.corr_names
+                for z in [self.unpack_site(self.loc, name).reshape(self.shapes[name]).flatten(0, len(self.corr_batch)-1).mean(0)]
+                for tr in [self.transforms[name]]
+            ), -1)
+        ) * torch.eye(self.corr_size, device=self.loc.device, dtype=self.loc.dtype)
+
     @property
     def corr_matrix(self):
         ret = self.loc.new_zeros(self.corr_batch + 2*(self.corr_size-1,))
         return ret.flatten(-2).scatter_(-1, self.corr_tril_indices, self.corr).unflatten(-1, ret.shape[-2:])
 
     @property
+    def zaux_corr(self):
+        return self.zaux_diag[..., self.corr_target_indices]
+
+    @property
     def _z_diag(self):
         ret = super()._z_diag
-        ret[..., self.corr_target_indices[..., 1:]] += (self.corr_matrix @ self.zaux_diag[..., self.corr_target_indices[..., :-1]].unsqueeze(-1)).squeeze(-1)
+        zaux = self.zaux_corr
+        ret[..., self.corr_target_indices[..., 1:]] += (self.corr_matrix @ zaux[..., :-1].unsqueeze(-1)).squeeze(-1)
+        ret[..., self.corr_target_indices] += (self.const_corr @ zaux.unsqueeze(-1)).squeeze(-1).sum(tuple(range(-1-len(self.corr_batch), -1)), keepdims=True) / self.corr_batch.numel()**0.5
         return ret
