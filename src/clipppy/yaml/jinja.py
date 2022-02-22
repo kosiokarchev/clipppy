a612b732e480b84889eb9eb10692fc9920abcb6e
Kosio Karchev <kosiokarchev@gmail.com>
2022-02-22 10:53:37 +0100

    jinja

---
+++ src/clipppy/yaml/jinja.py
@@ -0,0 +1,14 @@
+import io
+from os import PathLike
+from pathlib import Path
+from typing import Union
+
+from jinja2 import Environment, FileSystemLoader, StrictUndefined
+
+
+def parse(fname: Union[str, PathLike[str]], **kwargs):
+    fname = Path(fname)
+    return io.StringIO(Environment(
+        loader=FileSystemLoader(fname.parent),
+        undefined=StrictUndefined
+    ).from_string(open(fname).read()).render(**kwargs))
